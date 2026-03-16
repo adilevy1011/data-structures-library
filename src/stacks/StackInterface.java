caaf8f240174c3df6638cd276abe28fa084cce95
Adi Levy <adi.levy.1011@gmail.com>
2026-03-16 15:08:19 -0700

    stackInterface

---
+++ src/stacks/StackInterface.java
@@ -0,0 +1,11 @@
+package stacks;
+public interface StackInterface<T>{
+
+
+    public void push(T item);
+    public T pop();
+    public T peek();
+    public boolean isEmpty();
+    public int size();
+    public void clear();
+}
