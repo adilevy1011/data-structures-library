888d1434bbf933abe4d6c0a80b90af7742e73a09
Adi Levy <adi.levy.1011@gmail.com>
2026-03-18 15:46:13 -0700

    fixed queue

--- src/queues/ArrayQueue.java
+++ src/queues/ArrayQueue.java
@@ -7,73 +7,68 @@ public class ArrayQueue<T> implements QueueInterface<T> {
     private int back;
     private int size;
     private static final int DEFAULT_CAPACITY = 10;
-
+    
     @SuppressWarnings("unchecked")
-    public ArrayQueue() {
-        queue = (T[]) new Object[DEFAULT_CAPACITY];
+    public ArrayQueue(int capacity){
+        queue = (T[]) new Object[capacity];
         front = 0;
-        back = 0;
         size = 0;
+        back = capacity;
     }
-
-    @Override
-    public void enqueue(T item) {
-        if (size == queue.length) {
+    public ArrayQueue(){
+        this(DEFAULT_CAPACITY);
+    }
+    public void enqueue(T item){
+        if(size == queue.length){
             resize();
         }
-        queue[back] = item;
         back = (back + 1) % queue.length;
+        queue[back] = item;
         size++;
     }
-
-    @Override
-    public T dequeue() {
-        if (isEmpty()) {
+    
+    public T dequeue(){
+        if(isEmpty()){
             throw new IllegalStateException("Queue is empty");
         }
+        
         T item = queue[front];
-        queue[front] = null; 
+        queue[front] = null;
         front = (front + 1) % queue.length;
         size--;
         return item;
     }
-
-    @Override
-    public T peek() {
-        if (isEmpty()) {
+    
+    public T peek(){
+        if(isEmpty()){
             throw new IllegalStateException("Queue is empty");
         }
         return queue[front];
     }
-
-    @Override
-    public boolean isEmpty() {
+   
+    public boolean isEmpty(){
         return size == 0;
     }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public void clear() {
-        for (int i = 0; i < queue.length; i++) {
-            queue[i] = null; 
+     
+    public void clear(){
+        for(int i = 0; i < size; i++){
+            queue[(front + i) % queue.length] = null;
         }
         front = 0;
         back = 0;
         size = 0;
     }
+      
+    public int size(){
+        return size;
+    }
 
     @SuppressWarnings("unchecked")
-    private void resize() {
+    public void resize(){
         T[] newQueue = (T[]) new Object[queue.length * 2];
-        for (int i = 0; i < size; i++) {
+        for(int i = 0; i < queue.length; i++){
             newQueue[i] = queue[(front + i) % queue.length];
         }
         queue = newQueue;
-        front = 0;
-        back = size;
     }
 }
